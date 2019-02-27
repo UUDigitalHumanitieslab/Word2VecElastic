09499bcb7a204f0c64b2f698fbe6df19812cf671
BeritJanssen <berit.janssen@gmail.com>
2019-02-27 10:18:05 +0100

    sentences from Elasticsearch, plugged into runGenerateModels script

---
+++ collect_sentences.py
@@ -0,0 +1,128 @@
+from elasticsearch import Elasticsearch
+
+import string
+import re
+import nltk
+from nltk.corpus import words, wordnet, stopwords
+
+es = Elasticsearch()
+_englishWords = set(w.lower() for w in words.words())
+_englishStopWords = set(stopwords.words('english'))
+_removePunctuation = re.compile('[%s]' % re.escape(string.punctuation))
+
+class SentencesFromElasticsearch(object):
+    def __init__(self, minYear, maxYear):
+        self.minYear = minYear
+        self.maxYear = maxYear
+    def __iter__(self):
+        for year in range(self.minYear, self.maxYear+1):
+            documents = getDocumentsForYear(year)
+            for doc in documents:
+                sentences = _getSentencesInArticle(doc)
+                if not sentences:
+                    continue
+                for sentence in sentences:
+                    output = _prepareSentence(sentence)
+                    if output:
+                        yield output
+
+
+
+def getMinYear():
+    '''Retrieves the first year on the data set.'''
+    body = {
+        "aggs" : {
+            "min_date" : { "min" : { "field" : "date" } }
+        }
+    }
+    min_date = es.search(index='times', body=body, size=0)
+    #return int(min_date['aggregations']['min_date']['value_as_string'][:4])
+    return 1840 # returning fixed date for now
+
+def getMaxYear():
+    '''Retrieves the last year on the data set.'''
+    body = {
+        "aggs" : {
+            "max_date" : { "max" : { "field" : "date" } }
+        }
+    }
+    max_date = es.search(index='times', body=body, size=0)
+    #return int(max_date['aggregations']['max_date']['value_as_string'][:4])
+    return 1920 # returning fixed date for now
+
+def getDocumentsForYear(year):
+    '''Retrieves a list of documents for a year specified.'''
+    min_date = str(year)+"-01-01"
+    max_date = str(year)+"-12-31"
+    body = {
+        "query": {
+            "range" : {
+                "date" : {
+                    "gte" : min_date,
+                    "lte" : max_date
+                }
+            }
+        }
+    }
+    docs = es.search(index='times', body=body, size=1000, scroll="1m")
+    content = [result['_source']['content'] for result in docs['hits']['hits']]
+    total_hits = docs['hits']['total']
+    scroll_id = docs['_scroll_id']
+    while len(content)<total_hits:
+        if 'scroll_id' in docs:
+            scroll_id = docs['_scroll_id']
+        docs = es.scroll(scroll_id=scroll_id, scroll="1m")
+        content.extend([result['_source']['content'] for result in docs['hits']['hits']])
+    es.clear_scroll(scroll_id=scroll_id)
+    return content
+
+
+def getSentencesForYear(year):
+    '''Return list of lists of strings.
+    Return list of sentences in given year.
+    Each sentence is a list of words.
+    Each word is a string.'''
+    docs = getDocumentsForYear(year)
+    sentences = []
+    for doc in docs:
+        doc_tok = _getSentencesInArticle(doc)
+        if doc_tok:
+            sentences.extend(doc_tok)
+    final_sentences = [_prepareSentence(sentence) for sentence in sentences]
+    return final_sentences
+
+
+def _getSentencesInArticle(body):
+    """Transform a single news paper article into a list of sentences (each
+    sentence represented by a string)."""
+    sent_tokenizer = nltk.punkt.PunktSentenceTokenizer()
+    if isinstance(body, str):
+        sentences = sent_tokenizer.tokenize(body)
+        return sentences
+    else:
+        print(body)
+
+
+def _prepareSentence(sentence):
+    """Document preparation a sentence. Document preparation
+    consists of: removing punctuation, removing invalid words, lowe casing and
+    splitting into individual words."""
+    sentence = _removePunctuation.sub('', sentence) 
+    sentence = sentence.lower()
+    sentence = sentence.split(' ')
+    sentence = [w for w in sentence if _isValidWord(w)]
+    if len(sentence) > 0:
+        return sentence
+
+
+def _isValidWord(word):
+    """Determine whether a word is valid. A valid word is a valid english
+    non-stop word."""
+    if word in _englishStopWords:
+        return False
+    elif word in _englishWords:
+        return True
+    elif wordnet.synsets(word):
+        return True
+    else:
+        return False
---
+++ runGenerateModels.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+"""Generate a set of time shifting models from the given period of years. Each
+one of the models spans a given number of years.
+
+Usage:
+  runGenerateModels.py --y0=<y0> --yN=<yN> --nYears=<years> --outDir=<dir> [--step=<years>]
+
+Options:
+  --y0 <y0>         First year in the generated models
+  --yN <yN>         Last year in the generated models
+  --nYears <years>  Number of years per model
+  --outDir <dir>    Directory where models will be writen to
+  --step <years>    Step between start year of generated models [default: 1]
+"""
+import gensim
+
+from docopt import docopt
+from collect_sentences import SentencesFromElasticsearch
+from util import checkPath
+
+
+def generateModels(y0, yN, yearsInModel, stepYears, modelFolder):
+    """Generate time shifting w2v models on the given time range (y0 - yN).
+    Each model contains the specified number of years (yearsInModel). The start
+    year of each new model is set to be stepYears after the previous model.
+    Resulting models are saved on modelFolder.
+    """
+    checkPath(modelFolder)
+
+    for year in range(y0, yN - yearsInModel + 1, stepYears):
+        startY = year
+        endY = year + yearsInModel
+        modelName = modelFolder + '/%d_%d.w2v' % (year, year + yearsInModel)
+        vocabName = modelName.replace('.w2v', '.vocab.w2v')
+        print('Building model: ', modelName)
+
+        sentences = SentencesFromElasticsearch(startY, endY)
+        model = gensim.models.Word2Vec(min_count=1)
+        model.build_vocab(sentences)
+        model.train(sentences)
+
+        print('...saving')
+        model.init_sims(replace=True)
+        model.wv.save_word2vec_format(modelName, fvocab=vocabName, binary=True)
+
+
+if __name__ == '__main__':
+    args = docopt(__doc__)
+    yearsInModel = int(args['--nYears'])
+    stepYears = int(args['--step'])
+    outDir = args['--outDir']
+    y0 = int(args['--y0'])
+    yN = int(args['--yN'])
+
+    generateModels(y0, yN, yearsInModel, stepYears, outDir)
