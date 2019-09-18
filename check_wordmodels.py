31b3793cdf3767795aa3b563a421f6ad395997c2
BeritJanssen <berit.janssen@gmail.com>
2019-09-18 10:46:11 +0200

    added script to test word models for reference

---
+++ check_wordmodels.py
@@ -0,0 +1,6 @@
+from gensim.models import KeyedVectors
+
+def test_model(modelfile):
+    """ Given a .w2v modelfile, run tests to check if the word models make sense """
+    model = KeyedVectors.load_word2vec_format(modelfile, binary=True)
+    print(model.most_similar(positive=['woman', 'king'], negative=['man']))
