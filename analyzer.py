04735c2a7c994477f98dc64264f5659eaa25d067
BeritJanssen <berit.janssen@gmail.com>
2023-01-05 11:35:55 +0100

    add language models for german and french

--- analyzer.py
+++ analyzer.py
@@ -19,7 +19,9 @@ infixes = (
 )
 
 spacy_models = {
-    'English': "en_core_web_sm"
+    'english': "en_core_web_sm",
+    'german': "de_core_news_sm",
+    'french': "fr_core_news_sm"
 }
 
 class Analyzer(object):
